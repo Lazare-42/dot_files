47a19cedf7f00595c64b5a8f5ba18975a4571944
Lazare Rossillon <lazare@get-spoke.com>
2021-04-05 10:05:58 +0200

    update

--- execute.sh
+++ execute.sh
@@ -15,7 +15,6 @@ then
 fi
 
 mv ~/vimrc/.vimrc ~/
-mv ~/vimrc/.emacs ~/
 
 #Get Vim bundle manager
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
